f2f89b2cd7f03ce546049e6b80b4b951ed5bcd05
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-25 17:48:29 -0300

    Navbar Icons Component

---
+++ src/components/NavbarIcons/Icons.js
@@ -0,0 +1,13 @@
+import { Home } from '@styled-icons/boxicons-solid/Home'
+import { User as About } from '@styled-icons/boxicons-solid/User'
+import { Briefcase as Projects } from '@styled-icons/boxicons-solid/Briefcase'
+import { Chat as Contact } from '@styled-icons/boxicons-solid/Chat'
+
+const Icons = {
+  Home,
+  About,
+  Projects,
+  Contact,
+}
+
+export default Icons
---
+++ src/components/NavbarIcons/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'Home',
+    url: '/'
+  },
+  {
+    label: 'About',
+    url: '/about'
+  },
+  {
+    label: 'Projects',
+    url: '/projects'
+  },
+  {
+    label: 'Contact',
+    url: '/contact'
+  }
+]
+
+export default links
