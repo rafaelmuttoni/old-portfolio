99078e9b5fa534842969ebdde841799b9b931c36
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-25 15:23:42 -0300

    Setting up Global Styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import styled from 'styled-components'
+import GlobalStyles from '../../styles/global'
 
 const LayoutWrapper = styled.section`
   display: flex;
@@ -22,6 +23,7 @@ const LayoutMain = styled.main`
 const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
+      <GlobalStyles />
       <Navbar />
       <LayoutMain>{children}</LayoutMain>
     </LayoutWrapper>
