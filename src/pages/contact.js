ee4251faeb61da4be5aa75edc764a86cb3d09363
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-26 09:21:01 -0300

    Removed Lorem Ipsum in Index and added new pages

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact"/>
+    <h1>Contact Page</h1>
+  </Layout>
+)
+
+export default ContactPage
---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const ProjectsPage = () => (
+  <Layout>
+    <SEO title="Projects"/>
+    <h1>Projects Page</h1>
+  </Layout>
+)
+
+export default ProjectsPage
