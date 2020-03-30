79014fa178e97b2c47c573ad5e52e353f5d4ab2b
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-30 18:07:17 -0300

    Project description change

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -53,7 +53,7 @@ const Projects = () => {
         <S.ProjectsTitle>web developer projects <span role="img" aria-label="Laptop">💻</span></S.ProjectsTitle>
         <S.ProjectsDescription>latest projects I've worked on, you can <S.ProjectLink to="/contact" fade>check my social medias</S.ProjectLink> for more projects to come.</S.ProjectsDescription>
       </S.ProjectsWrapper>
-      <SingleProject title="muttoni.dev" url="https://muttoni.dev" description="This website's purpose is to serve me as my Resume and Portfolio. Website made from scratch with gatsby-starter-default." tags={['Front-end', 'Javascript', 'HTML5', 'CSS', 'React', 'Gatsby', 'GraphQL', 'styled-components']} dkImg={portfolioDkImg} mbImg={portfolioMbImg}/>
+      <SingleProject title="muttoni.dev" url="https://muttoni.dev" description="This website's purpose is to serve me as my Resume and Portfolio." tags={['Front-end', 'Javascript', 'HTML5', 'CSS', 'React', 'Gatsby', 'GraphQL', 'styled-components']} dkImg={portfolioDkImg} mbImg={portfolioMbImg}/>
       <SingleProject title="dynamicpoa.com" url="https://dynamicpoa.com" description="Website made for a company that offers solutions with Microsft's products (Office, PowerBI, etc.)." tags={['Front-end', 'Javascript', 'HTML5', 'CSS', 'React', 'Gatsby', 'Tailwindcss', 'Lottie']} dkImg={dynamicDkImg} mbImg={dynamicMbImg}/>
     </S.ContentWrapper>
   )
